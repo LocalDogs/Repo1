ca4d856e7630144e4a9af86fb8120f5a83d07d0f
IsabelHerrick <isabelherrick@yahoo.com>
2020-09-13 11:55:04 -0500

    Cardstack should be implemented, from Yuyakaido

---
+++ Codebase/app/src/main/java/com/example/localdogs/Cardstack.java
@@ -0,0 +1,14 @@
+package com.example.localdogs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Cardstack extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cardstack);
+    }
+}
