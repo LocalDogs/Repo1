584fde1d9d67448bbca3e933a45960f3ea4cbbdf
Trevor <tlockhart1@crimson.ua.edu>
2020-10-11 00:23:48 -0500

    jumpstart on dog and user class changes for next sprint. everything still works (at least on the current branch)

--- Codebase/app/src/main/java/com/example/localdogs/data/Dog.java
+++ Codebase/app/src/main/java/com/example/localdogs/data/Dog.java
@@ -1,47 +1,138 @@
 package com.example.localdogs.data;
 
+import android.util.Log;
+
 import androidx.annotation.NonNull;
 
 import com.google.gson.Gson;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 public class Dog {
 
     private String name;
-    private String breed;
-    private String dateofbirth;
+    private ArrayList<String> breeds;
+    private int age;
     private int weight;
+    private String owner;
+    private int activityLevel;
 
-    Dog (String name, String breed, String dateofbirth, int weight){
+    public Dog (String owner, String name, ArrayList<String> breeds, int age, int weight, int activityLevel){
         this.name = name;
-        this.breed = breed;
-        this.dateofbirth = dateofbirth;
+        this.owner = owner; //email of user
+        this.breeds = breeds;
+        this.age = age;
         this.weight = weight;
+        this.activityLevel = activityLevel;
     }
 
     public void setName(String name) { this.name = name; }
-    public void setBreed(String breed) { this.breed = breed; }
-    public void setDateOfBirth(String dateofbirth) { this.dateofbirth = dateofbirth; }
-    public void setWeight(int weight) { this.weight = weight; }
 
     public String getName() { return this.name; }
-    public String getBreed() { return this.breed; }
-    public String getDateOfBirth() { return this.dateofbirth; }
-    public int getWeight() { return this.weight; }
 
-    public String serializeToJson(){
-        Gson gson = new Gson();
-        return gson.toJson(this);
+    public String getOwner() {
+        return owner;
     }
 
-    public static Dog deserializeFromJson(String jsonDog){
-        Gson gson = new Gson();
-        return gson.fromJson(jsonDog, Dog.class);
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public int getActivityLevel() {
+        return activityLevel;
+    }
+
+    public void setActivityLevel(int activityLevel) {
+        this.activityLevel = activityLevel;
+    }
+
+    public void setWeight(int weight) { this.weight = weight; }
+
+    public int getWeight() { return this.weight; }
+
+    public void setBreeds(ArrayList<String> breeds) {
+        this.breeds = breeds;
+    }
+
+    public ArrayList<String> getBreeds() { return this.breeds; }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Boolean isMixed(){ return (breeds.size() > 2); }
+
+    /**
+     * This method is to turn a Dog class object into a json object.
+     * @param
+     * @return A JSONObject object representation of a Dog class object.
+     */
+    public JSONObject toJSONObject(){
+
+        JSONObject jsonUser = new JSONObject();
+        JSONArray dogArray = new JSONArray();
+        try {
+
+            jsonUser.put("owner", getOwner());
+            jsonUser.put("name", getName());
+            jsonUser.put("age", getAge());
+            jsonUser.put("activityLevel",getActivityLevel()); // temporary
+            for(String breed : breeds){
+
+                dogArray.put(breed);
+
+            }
+            jsonUser.put("breeds", dogArray);
+            jsonUser.put("mixed", isMixed());
+
+        } catch (JSONException e) {
+
+            e.printStackTrace();
+
+        }
+        return jsonUser;
+    }
+
+    public static Dog toDog(JSONObject jsonDog){
+
+        Dog dog = null;
+        try {
+            ArrayList<String> breedsList = new ArrayList<String>();
+            for(int i = 0; i < jsonDog.getJSONArray("breeds").length(); i++){
+
+                breedsList.add(jsonDog.getJSONArray("breeds").getString(i));
+
+            }
+            dog = new Dog
+                    (
+                            jsonDog.getString("owner"),
+                            jsonDog.getString("name"),
+                            breedsList,
+                            jsonDog.getInt("age"),
+                            jsonDog.getInt("weight"),
+                            jsonDog.getInt("activityLevel")
+                    );
+        } catch (JSONException e) {
+
+            e.printStackTrace();
+            Log.d("Dog Class", "Dog.toDog conversion error");
+
+        }
+        return dog;
     }
 
     @NonNull
     @Override
     public String toString(){
-        return "Dog: { name: " + this.name + " breed: " + this.breed + " dateofbirth: " + this.dateofbirth + " weight: " + this.weight + " }";
+        return this.toJSONObject().toString();
     }
 
 }
--- Codebase/app/src/main/java/com/example/localdogs/data/User.java
+++ Codebase/app/src/main/java/com/example/localdogs/data/User.java
@@ -4,8 +4,11 @@ import androidx.annotation.NonNull;
 
 import com.google.gson.Gson;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class User {
     private String firstname;
@@ -13,13 +16,33 @@ public class User {
     private String email;
     private String dateofbirth;
     private String password;
-
+    private ArrayList<Dog> dogs;
+    /*
+    TODO:
+        Update constructor calls in rest of code after sprint 1 presentation
+        for now, going to just overload the constructor for compatability
+     */
     public User(String firstname, String lastname, String email, String dateofbirth, String password){
         this.firstname = firstname;
         this.lastname = lastname;
         this.email = email;
         this.dateofbirth = dateofbirth;
         this.password = password;
+        ArrayList<String> breedList = new ArrayList<String>();
+        breedList.add("pickle");
+        breedList.add("rick");
+        ArrayList<Dog> dogList = new ArrayList<Dog>();
+        dogList.add(new Dog(email, "Cheerios", breedList, 3, 50, 2));
+        this.dogs = dogList;
+    }
+
+    public User(String firstname, String lastname, String email, String dateofbirth, String password, ArrayList<Dog> dogs){
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+        this.dateofbirth = dateofbirth;
+        this.password = password;
+        this.dogs = dogs;
     }
 
     public void setFirstName(String firstname){
@@ -52,16 +75,25 @@ public class User {
 
     public String getPassword() { return this.password; }
 
+    /**
+     * This method is to turn a User class object into a json object.
+     * @param
+     * @return A JSONObject object representation of a User class object.
+     */
     public JSONObject toJSONObject(){
 
         JSONObject jsonUser = new JSONObject();
+        JSONArray dogList = new JSONArray();
         try {
-
+            for(Dog dog : dogs){
+                dogList.put(dog.toJSONObject());
+            }
             jsonUser.put("firstname", getFirstname());
             jsonUser.put("lastname", getLastname());
             jsonUser.put("email", getEmail());
             jsonUser.put("password", getPassword()); // temporary
             jsonUser.put("dateofbirth", getDateOfBirth());
+            jsonUser.put("dogs", dogList);
 
         } catch (JSONException e) {
 
@@ -73,15 +105,23 @@ public class User {
 
     public static User toUser(JSONObject jsonUser){
         User user = null;
+        ArrayList<Dog> dogs = new ArrayList<Dog>();
         try {
+
+            for(int i = 0; i < jsonUser.getJSONArray("dogs").length(); i++){
+                Dog dog = Dog.toDog(jsonUser.getJSONArray("dogs").getJSONObject(i));
+                dogs.add(dog);
+            }
             user = new User
                     (
                             jsonUser.getString("firstname"),
                             jsonUser.getString("lastname"),
                             jsonUser.getString("email"),
                             jsonUser.getString("dateofbirth"),
-                            jsonUser.getString("password")
+                            jsonUser.getString("password")//,
+                            //dogs
             );
+
         } catch (JSONException e) {
             e.printStackTrace();
             System.out.println("User.toUser conversion error");
