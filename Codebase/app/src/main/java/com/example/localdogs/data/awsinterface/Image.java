ba2442b4f66b995edbab24af5b46b6c444d708c6
Trevor <tlockhart1@crimson.ua.edu>
2020-11-09 12:17:46 -0600

    adjusted image class

--- Codebase/app/src/main/java/com/example/localdogs/data/awsinterface/Image.java
+++ Codebase/app/src/main/java/com/example/localdogs/data/awsinterface/Image.java
@@ -1,6 +1,7 @@
 package com.example.localdogs.data.awsinterface;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.util.Log;
 
 import com.amazonaws.mobileconnectors.cognitoauth.Auth;
@@ -14,14 +15,31 @@ import com.example.localdogs.data.Dog;
 import com.example.localdogs.data.User;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 
 public abstract class Image {
 
-    public static void uploadImage(String dogName, File image, Consumer<StorageUploadFileResult> onSuccess, Consumer<StorageException> onFailure){
+    public static void uploadImage(Context context, String dogName, Bitmap image, Consumer<StorageUploadFileResult> onSuccess, Consumer<StorageException> onFailure) throws IOException {
+        upload(context, dogName, image, onSuccess, onFailure);
+    }
+
+    private static File convertImageToFile(String dogName, Context context, Bitmap image) throws IOException {
+        File imageFile = new File(context.getApplicationContext().getCacheDir(), dogName);
+        FileOutputStream outputStream = new FileOutputStream(imageFile);
+        image.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
+        outputStream.close();
+        return imageFile;
+    }
+
+    private static void upload(Context context, String dogName, Bitmap image, Consumer<StorageUploadFileResult> onSuccess, Consumer<StorageException> onFailure) throws IOException {
+        File imageFile = convertImageToFile(dogName, context.getApplicationContext(), image);
         User user = null;
         try {
             user = Authentication.getInstance().getCurrentSessionUser();
-            Amplify.Storage.uploadFile(user.getDogImageStorageKey(dogName), image, publicFileOptions(), success -> {
+            Amplify.Storage.uploadFile(user.getDogImageStorageKey(dogName), imageFile, publicFileOptions(), success -> {
 
                 Log.i("uploadImage", success.toString());
                 onSuccess.accept(success);
@@ -35,7 +53,6 @@ public abstract class Image {
         } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
 
     private static StorageUploadFileOptions publicFileOptions(){
