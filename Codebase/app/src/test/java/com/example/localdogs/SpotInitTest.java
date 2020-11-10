8afe0926e894e7611daad6c6000a762c0e24070c
IsabelHerrick <isabelherrick@yahoo.com>
2020-11-09 20:03:03 -0600

    finished spot test

--- Codebase/app/src/test/java/com/example/localdogs/SpotInitTest.java
+++ Codebase/app/src/test/java/com/example/localdogs/SpotInitTest.java
@@ -1,18 +1,20 @@
 package com.example.localdogs;
 
+import com.example.localdogs.data.Dog;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 public class SpotInitTest {
     private Spot s;
-    private String name;
-    private String city;
-    private String url;
-    private int id;
+    private Dog d;
 
     @Before
     public void setUp() throws Exception {
@@ -20,12 +22,18 @@ public class SpotInitTest {
 
     @Test
     public void spotConstructorTest() {
-        s = new Spot();
-        assertNull(s.name);
-        assertNull(s.city);
-        assertNull(s.url);
-        assertNotNull(s.id);
-
+        d = new Dog("Jane","Max", new ArrayList<String>(Arrays.asList("Pitbull", "Staffie")), new dob(12,31,2008), 50, 2, "https://i.imgur.com/E0MbHhU.jpg");
+        assertNotNull(d);
+        s = new Spot(d);
+        assertNotNull(s);
+    }
+    @Test
+    public void getNameTest(){
+        assert(s.getName().equals("Max"));
+    }
+    @Test
+    public void getTaglineTest(){
+        assert(s.getTagline().equals("Pitbull / Staffie [Mixed] | 18 years | 50 lbs | Energy Level: ★★☆☆☆"));
     }
     @After
     public void tearDown() throws Exception {
