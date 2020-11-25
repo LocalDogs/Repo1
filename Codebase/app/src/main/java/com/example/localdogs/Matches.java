5c2451bfcfde80303138ccd08ede82435d972154
Jake-UA <jswilliams25@crimson.ua.edu>
2020-11-24 23:08:04 -0600

    Registration Page/Matches Page/ Website Changes

--- Codebase/app/src/main/java/com/example/localdogs/Matches.java
+++ Codebase/app/src/main/java/com/example/localdogs/Matches.java
@@ -17,18 +17,5 @@ public class Matches extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_matches);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        CollapsingToolbarLayout toolBarLayout = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
-        toolBarLayout.setTitle(getTitle());
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 }
