536b5c854c4cc4e27612eee29dff4fedd9f72dd0
Jake-UA <jswilliams25@crimson.ua.edu>
2020-11-26 11:46:39 -0600

    Website format fixes

---
+++ docs/webpage.js
@@ -0,0 +1,7 @@
+window.onresize = function(event) {
+    var elem = document.getElementById("myDiv1");
+    elem.setAttribute("class","margin: 0px 10px;");
+
+    var elem = document.getElementById("myDiv2");
+    elem.setAttribute("class","margin: 0px 10px;");
+}
